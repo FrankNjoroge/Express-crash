bc940b1a7d66a1b8534df6a9821d8c995d5014a6
Frank Njoroge <fnjoro29@gmail.com>
2024-11-11 15:41:45 +0300

    setup static folder server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world </h1>");
-});
+//setup static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
 
 app.listen(8000, () => console.log(`Server running on port 8000`));
