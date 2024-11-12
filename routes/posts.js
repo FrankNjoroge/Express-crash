86aefda972e759b4e33904fd7fb3435bdfe590c1
Frank Njoroge <fnjoro29@gmail.com>
2024-11-12 23:51:31 +0300

    Refactor routes: Move post routes to separate router file

---
+++ routes/posts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const posts = [
+  { id: 1, title: "post one" },
+  { id: 2, title: "post two" },
+  { id: 3, title: "post three" },
+];
+
+// Get all posts
+router.get("/", (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
+});
+
+// Get single post
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({ message: `A post with ${id} was not found` });
+  }
+  res.status(200).json(post);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8000;
+const posts = require("./routes/posts");
 
 const app = express();
 
 //setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
 
-const posts = [
-  { id: 1, title: "post one" },
-  { id: 2, title: "post two" },
-  { id: 3, title: "post three" },
-];
-
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-  res.status(200).json(posts);
-});
-
-// Get single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    return res.status(404).json({ message: `A post with ${id} was not found` });
-  }
-  res.status(200).json(post);
-});
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
