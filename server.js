aa62e8080b11a1ef0027955bd25ae778ba0bdb62
Frank Njoroge <fnjoro29@gmail.com>
2024-11-12 23:33:51 +0300

    cleanup by removing else clauses within routes

--- server.js
+++ server.js
@@ -17,21 +17,19 @@ const posts = [
 app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
   if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
+    return res.status(200).json(posts.slice(0, limit));
   }
+  res.status(200).json(posts);
 });
 
 // Get single post
 app.get("/api/posts/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
-  if (post) {
-    res.status(200).json(post);
-  } else {
-    res.status(404).json({ message: "post not found" });
+  if (!post) {
+    return res.status(404).json({ message: `A post with ${id} was not found` });
   }
+  res.status(200).json(post);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
