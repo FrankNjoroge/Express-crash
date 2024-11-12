271d37c9b20d9ad20f95d175ed627156dafa31c0
Frank Njoroge <fnjoro29@gmail.com>
2024-11-12 23:18:53 +0300

    add functionality to handle getting a single as well as multiple posts

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const path = require("path");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 //setup static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-// app.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "about.html"));
-// });
+const posts = [
+  { id: 1, title: "post one" },
+  { id: 2, title: "post two" },
+  { id: 3, title: "post three" },
+];
 
-app.listen(8000, () => console.log(`Server running on port 8000`));
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    res.json(post);
+  } else {
+    res.statusCode = 404;
+    res.json({ message: "post not found" });
+  }
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
