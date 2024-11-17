ef9adef529bb648ad3179512b73f8fe79471c9b3
Frank Njoroge <fnjoro29@gmail.com>
2024-11-17 23:36:40 +0300

    add functionality to handle POST requests made from frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,52 @@
 const output = document.getElementById("output");
 const btn = document.getElementById("get-posts");
+const form = document.getElementById("add-post-form");
 
 btn.addEventListener("click", async () => {
-  const res = await fetch("http://localhost:8000/api/posts");
-  if (!res.ok) {
-    throw new Error("Failed to fetch");
+  try {
+    const res = await fetch("http://localhost:8000/api/posts");
+    if (!res.ok) {
+      throw new Error("Failed to fetch");
+    }
+    const posts = await res.json();
+    showPosts(posts);
+  } catch (error) {
+    console.log(error);
   }
-  const posts = await res.json();
-  showPosts(posts);
 });
 
 function showPosts(posts) {
-  output.innerHTML = posts.map((post) => {
-    return `<h1>${post.title}</h1>`;
+  output.innerHTML = "";
+  posts.forEach((post) => {
+    const div = document.createElement("div");
+    div.textContent = post.title;
+    output.appendChild(div);
   });
 }
+
+//submit new post
+async function addPost(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  const title = formData.get("title");
+  try {
+    const res = await fetch("http://localhost:8000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to add post");
+    }
+    const newPost = await res.json();
+    const postEl = document.createElement("div");
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+  } catch (error) {
+    console.error("Error adding post");
+  }
+}
+
+form.addEventListener("submit", addPost);
